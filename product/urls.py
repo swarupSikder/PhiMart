3eafa0eb22fe340334e80d0784209054cec71621
swarupSikder <swarupfx20@gmail.com>
2025-08-30 21:45:03 +0600

    Mixins & Generic API View

--- product/urls.py
+++ product/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from product import views
 
 urlpatterns = [
-    path('products/', views.ViewProducts.as_view(), name='product-list'),
+    path('products/', views.ProductList.as_view(), name='product-list'),
     path('product/<int:pk>/', views.ViewSingleProduct.as_view(), name='view-product'),
-    path('categories/', views.ViewCategories.as_view(), name='category-list'),
+    path('categories/', views.CategoryList.as_view(), name='category-list'),
     path('category/<int:pk>/', views.ViewSingleCategory.as_view(), name='view-category'),
 ]
--- product/views.py
+++ product/views.py
@@ -7,6 +7,15 @@ from product.models import Product, Category
 from product.serializers import ProductSerializer, CategorySerializer
 from django.db.models import Count
 from rest_framework.views import APIView
+from rest_framework.mixins import CreateModelMixin, ListModelMixin
+from rest_framework.generics import ListCreateAPIView
+
+
+
+
+
+
+
 
 # - - - - - - - - - - - #
 #      All Products     #
@@ -24,6 +33,20 @@ class ViewProducts(APIView):
         print(serializer.validated_data)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class ProductList(ListCreateAPIView):
+    queryset = Product.objects.select_related('category').all()
+    serializer_class = ProductSerializer
+
+    # def get_queryset(self):
+    #     return Product.objects.select_related('category').all()
+    
+    # def get_serializer_class(self):
+    #     return ProductSerializer
+
+    # def get_serializer_context(self):
+    #     return {'request': self.request}
 
 
 
@@ -65,6 +88,10 @@ class ViewCategories(APIView):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+
+class CategoryList(ListCreateAPIView):
+    queryset = Category.objects.annotate(product_count=Count('products')).all()
+    serializer_class = CategorySerializer
 
 
 
